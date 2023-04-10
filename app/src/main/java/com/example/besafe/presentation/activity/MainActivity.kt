3637f4b1fab8484f7e7bf82cf6cf58ce3e5dd425
Mustafa-Elazab <mustafa_elazabawy@yahoo.com>
2023-04-10 18:11:48 +0200

    add new apk

--- app/src/main/java/com/example/besafe/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/example/besafe/presentation/activity/MainActivity.kt
@@ -1,33 +1,15 @@
 package com.example.besafe.presentation.activity
 
-import android.content.Intent
-import android.net.Uri
+
 import android.os.Bundle
-import androidx.activity.viewModels
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.content.res.AppCompatResources
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.booleanPreferencesKey
-import androidx.datastore.preferences.core.edit
-import androidx.lifecycle.lifecycleScope
-import com.example.besafe.R
 import com.example.besafe.databinding.ActivityMainBinding
-import com.example.besafe.databinding.CustomDialogLayoutBinding
-import com.example.besafe.presentation.fragment.ChatViewModel
-import com.google.android.gms.ads.AdView
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.runBlocking
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
- 
+
     lateinit var binding: ActivityMainBinding
 
 
